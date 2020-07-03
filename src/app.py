4b45bab9a9fd8af9d9dbafdfdf5374281c8aaa2b
Caio Portela <caio_fabiio@outlook.com>
2020-07-02 22:44:21 -0300

    Add verification to authorization

--- src/app.py
+++ src/app.py
@@ -1,28 +1,45 @@
 import io
 import jwt
 from flask import Flask, jsonify, render_template, request, send_file
+from flask_httpauth import HTTPTokenAuth
 from openpyxl import load_workbook
 from PIL import Image
 
 app = Flask(__name__)
+auth = HTTPTokenAuth(scheme='Bearer')
 
-EMAILS = ["lucas@sheetgo.com", "mauricio@sheetgo.com", "rafael@sheetgo.com"]
+# Secret key to decode token
 SECRET = 'z6Ct_d2Wy0ZcZZVUYD3beI5ZCSsFrR6-f3ZDyn_MW00'
+
+# Accepted emails for authorization
+EMAILS = ['lucas@sheetgo.com', 'mauricio@sheetgo.com', 'rafael@sheetgo.com']
+
+@auth.verify_token
+def verify_token(token):
+    """Decode and verify the auth token."""
+    token_decoded = jwt.decode(token, SECRET)
+    email = token_decoded['email']
+
+    return email in EMAILS
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/excel/info', methods=['POST'])
+@auth.login_required
 def excel_info():
+    """Return the tabs from the excel file, ordered alphabetically."""
     sheet = request.files['file']
     sheet = load_workbook(sheet, read_only=True)
 
     sheetnames = sorted(sheet.sheetnames)
     return jsonify(sheetnames)
 
-@app.route('/image/convert', methods=['POST', 'GET'])
+@app.route('/image/convert', methods=['POST'])
+@auth.login_required
 def image_convert():
+    """Convert the format of an image."""
     format = request.form.get('format')
     file = request.files['file']
 
