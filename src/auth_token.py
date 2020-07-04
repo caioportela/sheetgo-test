31bf10e2dbb64b0c6f945a98a0bfc9bd4c30b4eb
Caio Portela <caio_fabiio@outlook.com>
2020-07-04 02:33:01 -0300

    Alter constant data

--- src/auth_token.py
+++ src/auth_token.py
@@ -1,11 +1,7 @@
 import jwt
 import secrets
 
-# Secret key to decode token
-SECRET = 'z6Ct_d2Wy0ZcZZVUYD3beI5ZCSsFrR6-f3ZDyn_MW00'
-
-# Accepted emails for authorization
-EMAILS = ['lucas@sheetgo.com', 'mauricio@sheetgo.com', 'rafael@sheetgo.com']
+from src.app import EMAILS, SECRET
 
 def generate():
     """Generate the auth token."""
